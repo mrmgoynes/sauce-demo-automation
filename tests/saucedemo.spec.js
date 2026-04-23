3dd83e0f2e56db99fdc9432cce119b9d506781fa
Michael <mrmichaelgoynes2@gmail.com>
2026-04-22 19:26:45 -0500

    feat: add negative login tests and refactor with beforeEach
    Added test cases for locked-out user, empty fields, and case sensitivity. Refactored Login suite to use a beforeEach hook for cleaner code.

--- tests/saucedemo.spec.js
+++ tests/saucedemo.spec.js
@@ -4,11 +4,15 @@ import { InventoryPage } from '../pages/InventoryPage';
 import { CartPage } from '../pages/CartPage';
 
 test.describe('Login Functionality', () => {
+    let loginPage;
+
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.goto();
+
+    });
 
     test('should login successfully with valid credentials', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-
-        await loginPage.goto();
         await loginPage.login('standard_user', 'secret_sauce');
 
         //Verify successful login
@@ -17,12 +21,42 @@ test.describe('Login Functionality', () => {
     });
 
     test('should show error with invalid credentials', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        await loginPage.goto();
         await loginPage.login('wrong_user', 'wrong_password');
 
         const errorContainer = page.locator('[data-test="error"]');
-        await expect(errorContainer).toContainText('Username and password do not match');
+        await expect(errorContainer).toContainText('Username and password do not match any user in this service');
+
+    });
+
+    test('should show error for  locked out user', async ({ page }) => {
+        await loginPage.login('locked_out_user', 'secret_sauce');
+
+        const errorContainer = page.locator('[data-test="error"]');
+        await expect(errorContainer).toContainText('Epic sadface: Sorry, this user has been locked out.');
+
+    });
+
+    test('should show error for empty username', async ({ page }) => {
+        await loginPage.login('', 'secret_sauce');
+
+        const errorContainer = page.locator('[data-test="error"]');
+        await expect(errorContainer).toContainText('Epic sadface: Username is required');
+
+    });
+
+    test('should show error for empty password', async ({ page }) => {
+        await loginPage.login('standard_user', '');
+
+        const errorContainer = page.locator('[data-test="error"]');
+        await expect(errorContainer).toContainText('Epic sadface: Password is required');
+
+    });
+
+    test('should be case sensitive for username', async ({ page }) => {
+        await loginPage.login('STANDARD_USER', 'secret_sauce');
+
+        const errorContainer = page.locator('[data-test="error"]');
+        await expect(errorContainer).toContainText('Epic sadface: Username and password do not match any user in this service');
     });
 
 });
